c6373a3c88b8dbf1d732afd101bb25c5fe8f3a65
SURAJ_KUMAR <surajk38656@gmail.com>
2025-02-26 20:21:58 +0530

    26-02-2025

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../Categories/Categories";
 import { Link, Outlet } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 import Crousel from "../Components/Crousel";
 import { useContext } from "react";
 import { myLoginContext } from "../Context/LoginContext";
 import axios from "axios";
 import BASE_URL from "../Config";
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import { Button } from "react-bootstrap";
+import { Slider } from "antd";
+
+let start =0;
+let end = 0;
+const onChange = (value) => {
+  start = value
+  console.log('onChange: ', start);
+};
+const onChangeComplete = (value) => {
+  end = value;
+  console.log("onChangeComplete: ", end);
+};
 
 const Home = () => {
   const { setIsLogedIn } = useContext(myLoginContext);
-
 
   const getProfile = async () => {
     const token = localStorage.getItem("token");
@@ -43,23 +56,53 @@ const Home = () => {
       <hr />
       <div id="hero">
         <div id="filter">
-          {/* <Form.Select>
-        <option>Filter Products</option>
-        <option>Laptops</option>
-        <option>Mobiles</option>
-        <option>TV</option>
-        <option>Keyboards</option>
-        <option>Mouse</option>
-        <option>Watches</option>
-        <option>Smart Watches</option>
-      </Form.Select> */}
-          {/*  */}
+          <b className="text-start">Fileter by price ₹</b>
+          <Button>
+            <i class="fas fa-circle-arrow-up"></i> Low To High{" "}
+            <i class="fas fa-circle-arrow-down"></i>
+          </Button>
+          <Button>
+            <i class="fas fa-circle-arrow-down"></i> High To Low{" "}
+            <i class="fas fa-circle-arrow-up"></i>
+          </Button>
+          <br />
+          <b>Filter by name</b>
+          <Form.Select
+            aria-label="Default select example"
+            style={{ backgroundColor: "#e8e6e6d8", fontWeight: "500" }}
+          >
+            <option>Select Category</option>
+            <option value="1">Keyboards</option>
+            <option value="1">Laptops</option>
+            <option value="2">Mobiles</option>
+            <option value="3">Mouse</option>
+            <option value="3">Smart Watches</option>
+            <option value="3">Watches</option>
+          </Form.Select>
+          <br />
+          <b>Price Range</b>
+          <Slider
+            range
+            step={1}
+            defaultValue={[20, 50]}
+            onChange={onChange}
+            onChangeComplete={onChangeComplete}
+            style={{width:'90%'}}
+          />
+
+          <br />
+          <b>Enter range</b>
+          <div id="range">
+            <input type="number" placeholder="0.00 ₹" />
+            TO
+            <input type="number" placeholder="1000000.00 ₹" />
+          </div>
         </div>
         <div id="filterdata">
           <Outlet />
         </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 };
